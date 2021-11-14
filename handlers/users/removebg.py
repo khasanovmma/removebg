30e199cf3af0bca01702b45d9306dad0db996a7d
Your Name <you@example.com>
2021-11-15 01:22:11 +0500

    test

--- handlers/users/removebg.py
+++ handlers/users/removebg.py
@@ -1,13 +1,23 @@
-from aiogram import types
+import os
+import time
 
-from loader import dp
+import requests
+from aiogram import types
+from loader import dp, bot
 from utils.photograph import photo_link
 
 
 @dp.message_handler(content_types='photo')
 async def photo_handler(msg: types.Message):
+    start = time.time()
+    chat_id = msg.from_user.id
     photo = msg.photo[-1]
     link = await photo_link(photo)
-    request_link = 'https://roughs.ru/api/remove-bg?url='
-    await msg.reply_document(request_link + link, caption="Bu fayl")
-    await msg.reply_photo(request_link + link, caption="Bu rasm")
+    image_content = requests.get(f'https://roughs.ru/api/remove-bg?url={link}').content
+    with open(f'image_{chat_id}.png', mode='wb') as file:
+        file.write(image_content)
+
+    with open(f'image_{chat_id}.png', mode='rb') as file:
+        await msg.reply_photo(file)
+        await msg.reply_document(file)
+    print(time.time() - start)
