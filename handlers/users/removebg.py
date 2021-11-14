1fbd5f92ef46a7d30ed6b069f6ebe87eff6e5852
Your Name <you@example.com>
2021-11-15 01:45:02 +0500

    test

--- handlers/users/removebg.py
+++ handlers/users/removebg.py
@@ -1,6 +1,9 @@
 import os
 import time
+from io import BytesIO
+from pprint import pprint
 
+import aiohttp
 import requests
 from aiogram import types
 from loader import dp, bot
@@ -17,7 +20,16 @@ async def photo_handler(msg: types.Message):
     with open(f'image_{chat_id}.png', mode='wb') as file:
         file.write(image_content)
 
-    with open(f'image_{chat_id}.png', mode='rb') as file:
-        await msg.reply_photo(file)
-        await msg.reply_document(file)
-    print(time.time() - start)
+    with open(f'image_{chat_id}.png' ) as file:
+        form = aiohttp.FormData()
+        form.add_field(
+            name='file',
+            value=file,
+        )
+        async with bot.session.post('https://telegra.ph/upload', data=form) as response:
+            img_src = await response.json()
+    pprint(img_src)
+    # with open(f'image_{chat_id}.png', mode='rb') as file:
+    #     await msg.reply_photo(file)
+    #     await msg.reply_document(document=file)
+    # print(time.time() - start)
